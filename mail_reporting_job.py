55cda094311800f748e85d1b191500c2fd7d30d3
zouguixing <zouguixing@statefortune.com.cn>
2019-08-07 15:48:18 +0800

    根据当前时间是 PM 还是 AM 分别发送对应的报表

--- mail_reporting_job.py
+++ mail_reporting_job.py
@@ -17,33 +17,38 @@ except ImportError:
 root_path = "/www/mpurselogs/doc"
 # 切换到报表目录
 os.chdir(root_path)
-# 报表A 每天上午6:00 发送  报表时间,都是发送前一天的
-ltime_A = (date.today() + timedelta(days=-1)).strftime("%d-%m-%Y")
-# 报表B 每天下午17:00 发送, 发送当天的
-ltime_B = (date.today()).strftime("%d-%m-%Y")
-print("time:" + ltime_A)
-if not os.path.exists(ltime_A):
+# 最终取时间
+ltime_N = None
+# 判断当前时间是 PM 还是 AM
+if time.localtime().tm_hour > 12:
+    # 下午取当天时间, 每天下午发送当天 17:00 前的报表B
+    ltime_N = (date.today()).strftime("%d-%m-%Y")
+else:
+    # 上午取昨日时间, 每天上午6:00 发送昨日整日的报表A.
+    ltime_N = (date.today() + timedelta(days=-1)).strftime("%d-%m-%Y")
+
+if not os.path.exists(ltime_N):
     # 当天没有文件, 终止程序.
     print("no file today.")
     sys.exit()
-zip_name = ltime_A + ".zip"
+zip_name = ltime_N + ".zip"
 if os.path.exists(zip_name):
     # 文件已存在,重命名备份
     os.rename(zip_name, "{}.{}".format(zip_name, time.strftime('%H%M%S')))
 # 压缩文件
 zip_file = zipfile.ZipFile(zip_name, "w")
-for f in os.listdir(ltime_A):
-    zip_file.write('./{}/{}'.format(ltime_A, f), compress_type=zipfile.ZIP_DEFLATED)
+for f in os.listdir(ltime_N):
+    zip_file.write('./{}/{}'.format(ltime_N, f), compress_type=zipfile.ZIP_DEFLATED)
 # 邮箱正文
-contents = ['{} daily report.  \n Mpurse Report '.format(ltime_A), ]
+contents = ['{} daily report.  \n Mpurse Report '.format(ltime_N), ]
 # 附件
 attachment = [zip_file.filename]
 # 邮件抬头
-subject = '{} Mpurse Report'.format(ltime_A)
+subject = '{} Mpurse Report'.format(ltime_N)
 # 接收地址
-# to = ['zouguixing@statefortune.com.cn', ]
-to = ['amarinder@mpurseservices.com', 'abinash.jha@mpurseservices.com', 'manoj.nigam@mpurseservices.com',
-      'zouguixing@statefortune.com.cn', 'praful.purohit@mpurseservices.com', 'oujiquan@statefortune.com.cn']
+to = ['zouguixing@statefortune.com.cn', ]
+# to = ['amarinder@mpurseservices.com', 'abinash.jha@mpurseservices.com', 'manoj.nigam@mpurseservices.com',
+#       'zouguixing@statefortune.com.cn', 'praful.purohit@mpurseservices.com', 'oujiquan@statefortune.com.cn']
 
 # 链接邮箱服务器
 mail_test = yagmail.SMTP(user="service@mpursewallet.com", password="qwerty@123", host='smtp.office365.com',
